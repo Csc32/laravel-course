49896fec3e13f6a9a3c098f038d61e14708fc96a
csc32 <sanzonettycar@gmail.com>
2023-09-12 19:05:32 -0400

    feat: add rol for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -21,6 +23,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        "rol",
         'password',
     ];
 
@@ -49,5 +52,9 @@ class User extends Authenticatable
     public function listings(): HasMany
     {
         return $this->hasMany(Listing::class, "user_id");
+    }
+    public function rol(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
     }
 }
