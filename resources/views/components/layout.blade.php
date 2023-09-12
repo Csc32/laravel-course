4411179d822d7bb75d5338ea7c7197d57fdf8c91
csc32 <sanzonettycar@gmail.com>
2023-09-12 19:04:07 -0400

    feat: add darkmode to web

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -60,7 +60,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
     {{-- VIEW OUTPUT --}}
     {{ $slot }}
 </body>
-<footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
+<footer class="fixed bottom-0 left-0 w-[100vw] flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
     <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
     <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -1,15 +1,14 @@
 @props(['tagsCsv'])
 
 @php
-    $tags = explode(',', $tagsCsv);
+$tags = explode(',', $tagsCsv);
 @endphp
 
-<ul class="flex">
+<ul class="flex items-center justify-content-center">
     @foreach ($tags as $tag)
-        <li
-            class="flex items-center justify-content-center bg-black text-white rounded-xl px-3 py-1 mr-2 dark:text-black dark:bg-white">
-            <a href="/?tag={{ $tag }}">{{ $tag }}</a>
-        </li>
+    <li class="rounded-xl px-3 py-1 mr-2  bg-black text-white dark:text-black dark:bg-white ">
+        <a href="/?tag={{ $tag }}">{{ $tag }}</a>
+    </li>
     @endforeach
 
 </ul>
